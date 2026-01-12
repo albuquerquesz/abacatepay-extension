88a434233d510fc8cc5ca5eba5fa549f1ae27fed
Victor Albuquerque <albq.victor@gmail.com>
2026-01-12 11:31:21 -0300

    initial commit from Abacatepay Extension

---
+++ src/panels/AbacatePayPanel.ts
@@ -0,0 +1,122 @@
+import * as vscode from "vscode";
+import { getUri } from "../utils/getUri";
+import { getNonce } from "../utils/getNonce";
+
+export class AbacatePayPanel {
+    public static currentPanel: AbacatePayPanel | undefined;
+    private readonly _panel: vscode.WebviewPanel;
+    private readonly _extensionUri: vscode.Uri;
+    private _disposables: vscode.Disposable[] = [];
+
+    private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+        this._panel = panel;
+        this._extensionUri = extensionUri;
+
+        // Set the webview's initial html content
+        this._panel.webview.html = this._getWebviewContent(this._panel.webview);
+
+        // Set up message listener
+        this._setWebviewMessageListener(this._panel.webview);
+
+        // Handle panel disposal
+        this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+    }
+
+    public static render(extensionUri: vscode.Uri) {
+        if (AbacatePayPanel.currentPanel) {
+            // If panel exists, reveal it
+            AbacatePayPanel.currentPanel._panel.reveal(vscode.ViewColumn.One);
+        } else {
+            // Create a new panel
+            const panel = vscode.window.createWebviewPanel(
+                "abacatepay",
+                "AbacatePay",
+                vscode.ViewColumn.One,
+                {
+                    enableScripts: true,
+                    retainContextWhenHidden: true,
+                    localResourceRoots: [
+                        vscode.Uri.joinPath(extensionUri, "out"),
+                        vscode.Uri.joinPath(extensionUri, "webview-ui", "dist"),
+                    ],
+                }
+            );
+
+            AbacatePayPanel.currentPanel = new AbacatePayPanel(panel, extensionUri);
+        }
+    }
+
+    public dispose() {
+        AbacatePayPanel.currentPanel = undefined;
+
+        this._panel.dispose();
+
+        while (this._disposables.length) {
+            const disposable = this._disposables.pop();
+            if (disposable) {
+                disposable.dispose();
+            }
+        }
+    }
+
+    private _getWebviewContent(webview: vscode.Webview): string {
+        const stylesUri = getUri(webview, this._extensionUri, [
+            "webview-ui",
+            "dist",
+            "assets",
+            "index.css",
+        ]);
+        const scriptUri = getUri(webview, this._extensionUri, [
+            "webview-ui",
+            "dist",
+            "assets",
+            "index.js",
+        ]);
+
+        const nonce = getNonce();
+
+        return /*html*/ `
+      <!DOCTYPE html>
+      <html lang="pt-BR">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}'; font-src ${webview.cspSource}; img-src ${webview.cspSource} https:; connect-src https:;">
+          <link rel="stylesheet" type="text/css" href="${stylesUri}">
+          <title>AbacatePay</title>
+        </head>
+        <body>
+          <div id="root"></div>
+          <script type="module" nonce="${nonce}" src="${scriptUri}"></script>
+        </body>
+      </html>
+    `;
+    }
+
+    private _setWebviewMessageListener(webview: vscode.Webview) {
+        webview.onDidReceiveMessage(
+            async (message: { command: string; data?: unknown }) => {
+                const { command, data } = message;
+
+                switch (command) {
+                    case "showInfo":
+                        vscode.window.showInformationMessage(data as string);
+                        break;
+                    case "showError":
+                        vscode.window.showErrorMessage(data as string);
+                        break;
+                    // Adicione mais handlers conforme necessário
+                    default:
+                        break;
+                }
+            },
+            undefined,
+            this._disposables
+        );
+    }
+
+    // Método para enviar mensagens para o webview
+    public postMessage(message: unknown) {
+        this._panel.webview.postMessage(message);
+    }
+}
